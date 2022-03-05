7e713612bda687eb5158811c9b1ec406ebd41a62
Captainographer <32444560+Captainographer@users.noreply.github.com>
2022-03-05 11:16:43 -0800

    fixed it

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ playerImg = pygame.image.load("spaceship.png")
 playerImg = pygame.transform.scale(playerImg, (70, 70))
 playerImg = pygame.transform.rotate(playerImg, 270)
 player = [20, 1080/2 - 35, 0, 0]
-planets = [];
+planets = []
 
 running = True
 
@@ -23,17 +23,17 @@ def addPlanet(imageName,size):
     planets.append([imageName,size])
 
 def importPlanets(planetArray): #planetArray must be a 2d array of planets
-    planetLocations = [];
-    loc = 0;  # x position of next planet; gets updated after every planet placement
+    planetLocations = []
+    loc = 0  # x position of next planet; gets updated after every planet placement
     for planet in planets:
         planetLocations.append([planet[0], planet[1], loc, 500])
-        loc = loc + (planet[0] * 2);
+        loc = loc + (planet[1] * 2)
     return planetLocations
 
 def drawPlanet(planet):
     planetImg = pygame.image.load(planet[0])
-    planetImg = pygame.transform.scale(playerImg, (planet[1], planet[1]))
-    playerImg = pygame.transform.rotate(playerImg, 270)
+    planetImg = pygame.transform.scale(planetImg, (planet[1], planet[1]))
+    planetImg = pygame.transform.rotate(planetImg, 270)
     screen.blit(planetImg,(planet[2],planet[3]))
 
 
@@ -83,8 +83,8 @@ while running:
     drawBackground()
     print("working")
     drawPlayer(player[0], player[1])
-    addPlanet("planet.png",100);
-    addPlanet("planet.png",50);
+    addPlanet("planet.png",100)
+    addPlanet("planet.png",50)
     setup(importPlanets(planets))
     print("skipped")
     pygame.display.update()
